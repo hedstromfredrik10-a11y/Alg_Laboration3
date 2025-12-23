ed36df04b1fa0ee45302a96a3be52506f7233652
hedstromfredrik10-a11y <hedstromfredrik10@gmail.com>
2025-12-23 22:12:08 +0100

    HeapSorter

--- Lab3/src/se/hig/aod/lab3/HeapSorter.java
+++ Lab3/src/se/hig/aod/lab3/HeapSorter.java
@@ -1,5 +1,34 @@
 package se.hig.aod.lab3;
 
-public class HeapSorter {
-    
+import java.util.ArrayList;
+import java.util.List;
+
+public class HeapSorter<T extends Comparable<? super T>> {
+
+    public static void main(String[] args) {
+        int[] arr = { 1, 5, 2, 8, 9, 3, 8, 2, 11 };
+        List<Integer> inputList = new ArrayList<>();
+        for (Integer integer : arr) {
+            inputList.add(integer);
+        }
+        System.out.println(sort(inputList).toString());
+    }
+
+    public static <T extends Comparable<? super T>> List<T> sort(List<T> inputList) {
+        HeapPriorityQueue<T> heap = new HeapPriorityQueue<>(inputList.size());
+
+        for (T element : inputList) {
+            heap.enqueue(element);
+        }
+
+        List<T> sortedList = new ArrayList<>();
+
+        while (!heap.isEmpty()) {
+            sortedList.add(heap.dequeue());
+        }
+
+        return sortedList;
+
+    }
+
 }
